d69ad0ef75cd2684dab200edb40c0880cf6243b2
sayasurvey <sayasurvey@yahoo.co.jp>
2025-05-25 17:34:59 +0900

    FIX: ページネーションをコンポーネントに切り出し

--- src/src/app/ui/books/BookList.tsx
+++ src/src/app/ui/books/BookList.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import { fetcher } from '../../../../lib/utils';
 import { BookCard } from './BookCard';
+import { Pagination } from '../utils/Pagination';
 
 interface Book {
   id: number;
@@ -31,7 +32,7 @@ interface BooksResponse {
   perPage: number;
 }
 
-const PER_PAGE = 1;
+const PER_PAGE = 50;
 
 export const BookList: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
@@ -75,42 +76,11 @@ export const BookList: React.FC = () => {
         </div>
         
         <div className="mt-8 flex justify-center">
-          <ul className="Pagination">
-            <li className="Pagination-Item">
-              <button
-                onClick={() => handlePageChange(currentPage - 1)}
-                disabled={currentPage === 1}
-                className="Pagination-Item-Link"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="Pagination-Item-Link-Icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
-                </svg>
-              </button>
-            </li>
-            
-            {Array.from({ length: lastPage }, (_, i) => i + 1).map((page) => (
-              <li key={page} className="Pagination-Item">
-                <button
-                  onClick={() => handlePageChange(page)}
-                  className={`Pagination-Item-Link ${currentPage === page ? 'isActive' : ''}`}
-                >
-                  <span>{page}</span>
-                </button>
-              </li>
-            ))}
-            
-            <li className="Pagination-Item">
-              <button
-                onClick={() => handlePageChange(currentPage + 1)}
-                disabled={currentPage === lastPage}
-                className="Pagination-Item-Link"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="Pagination-Item-Link-Icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
-                </svg>
-              </button>
-            </li>
-          </ul>
+          <Pagination
+            currentPage={currentPage}
+            lastPage={lastPage}
+            onPageChange={handlePageChange}
+          />
         </div>
       </div>
     </div>
